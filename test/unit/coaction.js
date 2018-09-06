2ee75f9abe825cab7abea6d794fc49cd64752232
Ivo Havener <ivolucien@gmail.com>
2018-09-06 12:29:24 -0700

    initial structure

---
+++ test/unit/coaction.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const mocha = require('mocha')
+const chai = require('chai')
+
+describe('Empty test', function () {
+  it('does nothing', function () {
+    console.log('@todo: add proper logger support')
+  })
+})
