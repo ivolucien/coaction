24ec937c25ef85dbcef2a1161b05752439f2f395
Ivo Havener <ivolucien@gmail.com>
2018-09-16 09:37:03 -0700

    client access related test updates

--- test/unit/coaction.js
+++ test/unit/coaction.js
@@ -4,6 +4,8 @@ const CoAction = require('../../lib/coaction')
 const expect = require('chai').expect
 const Db = require('../fixtures/db')
 
+// CoAction helps keep data from multiple sources synchronized
+// This class just manages clients
 describe('CoAction', function () {
   context('#constructor', function () {
     const db = new Db()
@@ -17,91 +19,92 @@ describe('CoAction', function () {
       })
     })
 
-    context('with a object-mapped sources argument', function () {
-      it('should contain matching sources', function () {
+    context('with a object-mapped clients argument', function () {
+      it('should contain matching clients', function () {
         const co = new CoAction(map)
         expect(co).to.be.an('object')
         expect(co.size).to.equal(Object.keys(map).length, 'size')
-        expect(co.getSources()).to.include(db, 'mock db')
+        expect(co.clients).to.be.an('object', 'client map')
+        expect(Object.keys(co.clients)[0]).to.equal(Object.keys(map)[0], JSON.stringify(map))
       })
     })
 
-    context('with a client property in the sources argument', function () {
+    context('with a client property in the clients argument', function () {
       it('supports access to the client by name', function () {
         const co = new CoAction(map)
-        expect(co.sources.mock).to.be.an('object', 'db mock')
-        expect(co.sources.mock.commit).to.be.a('function', 'db mock commit()')
+        expect(co.clients.mock).to.be.an('object', 'db mock')
+        expect(co.clients.mock.commit).to.be.a('function', 'db mock commit()')
       })
     })
   })
 
   // @todo: consolidate '#transaction', '#commit' and 'rollback' contexts into one loop?
   context('#transaction', function () {
-    context('when initialized with a source client', function () {
-      const sources = { mockA: new Db() }
+    context('when initialized with a client', function () {
+      const clients = { mockA: new Db() }
       it('should delegate to that client', async function () {
-        const co = new CoAction(sources)
+        const co = new CoAction(clients)
         expect(co).to.have.property('transaction')
         await co.transaction()
-        expect(co.sources.mockA.state).to.equal('transacting')
+        expect(co.clients.mockA.state).to.equal('transacting')
       })
     })
 
-    context('when initialized with multiple source clients', function () {
-      const sources = { mockA: new Db(), mockB: new Db() }
+    context('when initialized with multiple clients', function () {
+      const clients = { mockA: new Db(), mockB: new Db() }
       it('should start a new transaction for all clients', async function () {
-        const co = new CoAction(sources)
+        const co = new CoAction(clients)
         await co.transaction()
-        expect(co.sources.mockA.state).to.equal('transacting', 'mockA')
-        expect(co.sources.mockB.state).to.equal('transacting', 'mockB')
+        expect(co.clients.mockA.state).to.equal('transacting', 'mockA')
+        expect(co.clients.mockB.state).to.equal('transacting', 'mockB')
       })
     })
   })
 
   context('#commit', function () {
-    context('when initialized with a source client', function () {
-      const sources = { mockA: new Db() }
+    context('when initialized with a client', function () {
+      const clients = { mockA: new Db() }
       it('should delegate to that client', async function () {
-        const co = new CoAction(sources)
+        const co = new CoAction(clients)
         expect(co).to.have.property('commit')
         await co.transaction()
         await co.commit()
-        expect(co.sources.mockA.state).to.equal('committed')
+        expect(co.clients.mockA.state).to.equal('committed')
       })
     })
 
-    context('when initialized with multiple source clients', function () {
-      const sources = { mockA: new Db(), mockB: new Db() }
+    context('when initialized with multiple clients', function () {
+      const clients = { mockA: new Db(), mockB: new Db() }
       it('should commit all clients', async function () {
-        const co = new CoAction(sources)
+        const co = new CoAction(clients)
         await co.transaction()
         await co.commit()
-        expect(co.sources.mockA.state).to.equal('committed', 'mockA')
-        expect(co.sources.mockB.state).to.equal('committed', 'mockB')
+        expect(co.clients.mockA.state).to.equal('committed', 'mockA')
+        expect(co.clients.mockB.state).to.equal('committed', 'mockB')
       })
     })
   })
 
   context('#rollback', function () {
-    context('when initialized with a source client', function () {
-      const sources = { mockA: new Db() }
+    context('when initialized with a client', function () {
+      const clients = { mockA: new Db() }
       it('should delegate to that client', async function () {
-        const co = new CoAction(sources)
+        const co = new CoAction(clients)
         expect(co).to.have.property('rollback')
         await co.transaction()
         await co.rollback()
-        expect(co.sources.mockA.state).to.equal('rolledback')
+        expect(co.clients.mockA.state).to.equal('rolledback')
       })
     })
 
-    context('when initialized with multiple source clients', function () {
-      const sources = { mockA: new Db(), mockB: new Db() }
+    context('when initialized with multiple clients', function () {
+      const clients = { mockA: new Db(), mockB: new Db() }
       it('should rollback all clients', async function () {
-        const co = new CoAction(sources)
+        const co = new CoAction(clients)
         await co.transaction()
         await co.rollback()
-        expect(co.sources.mockA.state).to.equal('rolledback', 'mockA')
-        expect(co.sources.mockB.state).to.equal('rolledback', 'mockB')
+        expect(co.clients.mockA.state).to.equal('rolledback', 'mockA')
+        expect(co.clients.mockB.state).to.equal('rolledback', 'mockB')
       })
     })
   })
